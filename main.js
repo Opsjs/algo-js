340879c4c6e7540688c1b37b66358079f1e88768
Opsjs <ugosignat@gmail.com>
2023-10-10 12:55:24 +0200

    exo tableau

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-alert("test");
+let fruits = ['pomme', 'poire', 'banane'];
+
+fruits.forEach(function(fruit){
+    console.log(fruit);
+});
+
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
