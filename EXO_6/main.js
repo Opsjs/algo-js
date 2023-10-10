4ed434d9785264d2519bc94037862d0da06f0cf9
Opsjs <ugosignat@gmail.com>
2023-10-10 10:49:53 +0200

    ajout exo 6

---
+++ EXO_6/main.js
@@ -0,0 +1,13 @@
+let a = 'Jean';
+let b = 'Paul';
+
+function CheckNames(Name1, Name2) {
+    if (Name1==Name2) {
+        return true;
+    } else {
+        return false;
+    };
+};
+
+let result = CheckNames(a, b);
+console.log(result)
